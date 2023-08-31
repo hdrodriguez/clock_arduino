ca404ef7d3d00b61704696a908b0af88457cddbc
Hernan Rodriguez <herodsoft@gmail.com>
2023-08-31 14:39:46 -0500

    Update leds_clock.ino
    Se añaden numeros de 0 al 9

--- leds_clock.ino
+++ leds_clock.ino
@@ -1,96 +1,124 @@
-#define A  3
-#define F0 4
-#define F1 5
-#define G  6
-#define E0 9
-#define E1 10
-#define D  11
+
+#define a 3
+#define b 4
+#define c 5
+#define d 6
+#define e 9
+#define f 10
+#define g 11
+
+int nums[][5][7] = {
+      {
+        {1,1,1,1,1,1,1}, {1,0,0,0,0,0,1}, {1,0,0,0,0,0,1}, {1,1,1,1,1,1,1}, {0,0,0,0,0,0,0} //0
+      },
+      {
+        {0,0,0,0,0,0,0}, {1,0,0,0,0,0,0}, {1,1,1,1,1,1,1}, {0,0,0,0,0,0,0}, {0,0,0,0,0,0,0} //1
+      },
+      {
+        {1,0,0,1,1,1,1}, {1,0,0,1,1,1,1}, {1,0,0,1,1,1,1}, {1,1,1,1,0,0,1}, {0,0,0,0,0,0,0} //2
+      },
+      {
+        {1,0,0,1,0,0,1}, {1,0,0,1,0,0,1}, {1,0,0,1,0,0,1}, {1,1,1,1,1,1,1}, {0,0,0,0,0,0,0} //3
+      },
+      {
+        {1,1,1,1,0,0,0}, {0,0,0,1,0,0,0}, {0,0,0,1,0,0,0}, {1,1,1,1,1,1,1}, {0,0,0,0,0,0,0} //4
+      },
+      {
+        {1,1,1,1,0,0,1}, {1,1,1,1,0,0,1}, {1,1,1,1,0,0,1}, {1,0,0,1,1,1,1}, {0,0,0,0,0,0,0} //5
+      },
+      {
+        {1,1,1,1,1,1,1}, {1,0,0,1,0,0,1}, {1,0,0,1,0,0,1}, {1,0,0,1,1,1,1}, {0,0,0,0,0,0,0} //6
+      },
+      {
+        {1,0,0,0,0,0,0}, {1,0,0,0,0,0,0}, {1,0,0,0,0,0,0}, {1,1,1,1,1,1,1}, {0,0,0,0,0,0,0} //7
+      },
+      {
+        {1,1,1,1,1,1,1}, {1,0,0,1,0,0,1}, {1,0,0,1,0,0,1}, {1,1,1,1,1,1,1}, {0,0,0,0,0,0,0} //8
+      },
+      {
+        {1,1,1,1,0,0,0}, {1,0,0,1,0,0,0}, {1,0,0,1,0,0,0}, {1,1,1,1,1,1,1}, {0,0,0,0,0,0,0} //9
+      }
+  };
+
+int hz = 0;
 
 
-int active = 0;
-
-int a = HIGH;
-int l = LOW;
-int f =0;
 
 
-void convert(int num)
+void draw(int num[5][7])
 {
-  f = 500;
-
-  if(num == 0)
-  {
-        digitalWrite(A, l);
-        digitalWrite(F0, a);
-        digitalWrite(F1, a);
-        digitalWrite(G, l);
-        digitalWrite(E0, a);
-        digitalWrite(E1, a);
-        digitalWrite(D, l);
-
-        delay(f);
-
-
-        digitalWrite(A, a);
-        digitalWrite(F0, l);
-        digitalWrite(F1, l);
-        digitalWrite(G, a);
-        digitalWrite(E0, l);
-        digitalWrite(E1, l);
-        digitalWrite(D, a);
-
-        delay(f);
+  hz = 30;
+  for(int i = 0; i<=4; i++)
+    {
       
-
-        digitalWrite(A, a);
-        digitalWrite(F0, l);
-        digitalWrite(F1, l);
-        digitalWrite(G, a);
-        digitalWrite(E0, l);
-        digitalWrite(E1, l);
-        digitalWrite(D, a);
+      for(int j = 0; j<=6; j++)
+      {
        
-        delay(f);
+        if(j == 0)
+        {
+          digitalWrite(a, num[i][j]);
+        }
 
-        digitalWrite(A, a);
-        digitalWrite(F0, l);
-        digitalWrite(F1, l);
-        digitalWrite(G, a);
-        digitalWrite(E0, l);
-        digitalWrite(E1, l);
-        digitalWrite(D, a);
-        
-        delay(f);
-        digitalWrite(A, l);
-        digitalWrite(F0, a);
-        digitalWrite(F1, a);
-        digitalWrite(G, l);
-        digitalWrite(E0, a);
-        digitalWrite(E1, a);
-        digitalWrite(D, l);
-        delay(1000);     
-        
-  }
+        if(j == 1)
+        {
+          digitalWrite(b, num[i][j]);
+        }
+
+        if(j == 2)
+        {
+          digitalWrite(c, num[i][j]);
+        }
+
+        if(j == 3)
+        {
+          digitalWrite(d, num[i][j]);
+        }
+
+        if(j == 4)
+        {
+          digitalWrite(e, num[i][j]);
+        }
+
+        if(j == 5)
+        {
+          digitalWrite(f, num[i][j]);
+        }
+
+        if(j == 6)
+        {
+          digitalWrite(g, num[i][j]);
+        }
+      }
+      delay(hz);
+    }
+
+    
 
 }
 
 void setup() {
   // put your setup code here, to run once:
 
-
-  pinMode(A, OUTPUT);
-  pinMode(F0, OUTPUT);
-  pinMode(F1, OUTPUT);
-  pinMode(G, OUTPUT);
-  pinMode(E0, OUTPUT);
-  pinMode(E1, OUTPUT);
-  pinMode(D, OUTPUT);
+  Serial.begin(9600);
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(f, OUTPUT);
+  pinMode(g, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  convert(0);
+  int imax = sizeof(nums)/sizeof(nums[0]);
+  for(int k = 0; k<imax; k++)
+  {
+    draw(nums[k]);
+  }
+  
+  
 
 }
